d8582546b6aaf5c2d6d5951495f71097a2bcf141
Ivaylo Georgiev <ivogeorgiev@hotmail.com>
2024-01-15 10:10:42 +0000

    Add project files.

---
+++ AuthServices/Constants.cs
@@ -0,0 +1,14 @@
+﻿namespace AuthServices;
+
+public static class Constants
+{
+	public static readonly string ClientId = "8073202f-dbe3-4634-8992-6a72d6420c65"; // "YOUR_CLIENT_ID_HERE"
+	public static readonly string[] Scopes = { "openid", "offline_access" };
+	// The next code to add B2C
+	public static readonly string TenantName = "MauiAd";
+	public static readonly string TenantId = $"{TenantName}.onmicrosoft.com";
+	public static readonly string SignInPolicy = "B2C_1_social";
+	public static readonly string AuthorityBase = $"https://{TenantName}.b2clogin.com/tfp/{TenantId}/";
+	public static readonly string AuthoritySignIn = $"{AuthorityBase}{SignInPolicy}";
+	public static readonly string IosKeychainSecurityGroups = "com.microsoft.adalcache";
+}
---
+++ MauiAuth/Platforms/Android/MainActivity.cs
@@ -0,0 +1,26 @@
+﻿using Android.App;
+using Android.Content;
+using Android.Content.PM;
+using Android.OS;
+using Microsoft.Identity.Client;
+
+namespace MauiAuth
+{
+    [Activity(Theme = "@style/Maui.SplashTheme",
+    MainLauncher = true,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density,
+    SupportsPictureInPicture = true,
+    HardwareAccelerated = true,
+    LaunchMode = LaunchMode.SingleInstance,
+    Exported = true,
+    ResizeableActivity = true,
+    AllowEmbedded = false)]
+    public class MainActivity : MauiAppCompatActivity
+    {
+        protected override void OnActivityResult(int requestCode, Result resultCode, Intent? data)
+        {
+            base.OnActivityResult(requestCode, resultCode, data);
+            AuthenticationContinuationHelper.SetAuthenticationContinuationEventArgs(requestCode, resultCode, data);
+        }
+    }
+}
